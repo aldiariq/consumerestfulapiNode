44846eeec283bc6fd3e03d6b0cd73c1e5970621e
aldiariq <aldiariq@github.com>
2020-12-22 21:20:23 +0700

    selesai membuat navigasi, desain halaman masuk dan daftar

--- app/src/main/java/com/aldiariq/restfulconsume/MainActivity.kt
+++ app/src/main/java/com/aldiariq/restfulconsume/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.aldiariq.restfulconsume
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aldiariq.restfulconsume.autentikasi.AutentikasiActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+        startActivity(Intent(this, AutentikasiActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/aldiariq/restfulconsume/autentikasi/AutentikasiActivity.kt
@@ -0,0 +1,12 @@
+package com.aldiariq.restfulconsume.autentikasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldiariq.restfulconsume.R
+
+class AutentikasiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_autentikasi)
+    }
+}
